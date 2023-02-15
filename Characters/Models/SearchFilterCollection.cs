88ed7170e1a5ab5fb30cdb3ddbe8ef4b546b52f9
derpy-solutions <102288477+KenediaDev@users.noreply.github.com>
2023-02-15 23:07:27 +0100

    file moved

---
+++ Characters/Models/SearchFilterCollection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Kenedia.Modules.Characters.Models
+{
+    public class SearchFilterCollection : Dictionary<string, SearchFilter<Character_Model>>
+    {
+    }
+}
--- Characters/SearchFilterCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using Kenedia.Modules.Characters.Models;
-using System.Collections.Generic;
-
-namespace Kenedia.Modules.Characters
-{
-    public class SearchFilterCollection : Dictionary<string, SearchFilter<Character_Model>>
-    {
-    }
-}
