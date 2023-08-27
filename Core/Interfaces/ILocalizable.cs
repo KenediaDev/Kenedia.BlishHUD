1b267ead7d5e103982346c6f5b30314e1b5b982c
KenediaDev <102288477+KenediaDev@users.noreply.github.com>
2023-08-27 10:32:29 +0200

    Delete Core directory

--- Core/Interfaces/ILocalizable.cs
+++
@@ -1,9 +0,0 @@
-﻿using Blish_HUD;
-
-namespace Kenedia.Modules.Core.Interfaces
-{
-    public interface ILocalizable
-    {
-       void UserLocale_SettingChanged(object sender = null, ValueChangedEventArgs<Gw2Sharp.WebApi.Locale> e = null);
-    }
-}
